27dfd93b1cb7c82bdb694dad1c351a7b6a2aedc2
John-laptop <johndog3112@gmail.com>
2024-01-25 17:32:01 -0600

    seq 01

--- main.rs
+++ main.rs
@@ -6,49 +6,10 @@
 // To run the code:
 //     $ cargo run
 
-use derive_debug::CustomDebug;
-use std::fmt::Debug;
+use seq::seq;
 
-pub trait Trait {
-    type Value;
-}
+seq!(N in 0..8 {
+    // nothing
+});
 
-#[derive(CustomDebug)]
-#[debug(bound = "T::Value: Debug")]
-pub struct Wrapper<T: Trait> {
-    field: Field<T>,
-    #[debug = "0b{:08b}"]
-    bitmask: u8,
-}
-
-#[derive(CustomDebug)]
-pub struct Wrapper2<T: Trait, U> {
-    #[debug(bound = "T::Value: Debug")]
-    field: Field<T>,
-    #[debug = "0b{:08b}"]
-    bitmask: u8,
-    extra: U,
-}
-
-#[derive(CustomDebug)]
-struct Field<T: Trait> {
-    values: Vec<T::Value>,
-}
-
-fn assert_debug<F: Debug>() {}
-
-fn main() {
-    struct Id;
-
-    impl Trait for Id {
-        type Value = u8;
-    }
-
-    assert_debug::<Wrapper<Id>>();
-    assert_debug::<Wrapper2<Id, u8>>();
-}
-
-
-
-
-
+fn main() {}
--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,42 @@
 use proc_macro::TokenStream;
+use syn::{Result, Token, parse_macro_input, parse::Parse};
+
+
+#[allow(dead_code)]
+struct Seq {
+    var: syn::Ident,
+    lower: syn::LitInt,
+    upper: syn::LitInt,
+    block: proc_macro2::TokenStream
+}
+impl Parse for Seq {
+    fn parse(input: syn::parse::ParseStream) -> Result<Self> {
+        let var: syn::Ident = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let lower: syn::LitInt = input.parse()?;
+        input.parse::<Token![..]>()?;
+        let upper: syn::LitInt = input.parse()?;
+        let block: proc_macro2::TokenStream = input.parse()?;
+        Ok(Self {
+            var,
+            lower,
+            upper,
+            block
+        })
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    
+    let Seq {
+        var,
+        lower,
+        upper,
+        block
+    } = parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    eprintln!("{}, {}, {}, {}", var, lower, upper, block);
+    
+    TokenStream::new()
 }
