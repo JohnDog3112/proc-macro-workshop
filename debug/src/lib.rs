be7317529ecf30fe6f32bd4c92a806c4884cadc1
John-laptop <johndog3112@gmail.com>
2024-01-25 15:35:31 -0600

    Debug 08

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,8 @@
 use std::collections::{HashSet, HashMap};
 
-use proc_macro2::TokenStream;
+use proc_macro2::{TokenStream, TokenTree, Span};
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, DeriveInput, Data, DataStruct, Result, Error, Meta, Expr, spanned::Spanned, Lit, Type, PathArguments, Generics, Fields, GenericArgument};
+use syn::{parse_macro_input, DeriveInput, Data, DataStruct, Result, Error, Meta, Expr, spanned::Spanned, Lit, Type, PathArguments, Generics, Fields, GenericArgument, LitStr};
 
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
@@ -22,32 +22,65 @@ fn derive_impl(input: DeriveInput) -> Result<TokenStream> {
     let name_str = name.to_string();
 
 
-    let (implementation, bound_generics) = if let Data::Struct(val) = &input.data {
+    let (implementation, fields) = if let Data::Struct(val) = &input.data {
         (
             derive_struct(&name_str, val)?, 
-            get_uses_of_generics(&input.generics, &val.fields)?
+            &val.fields
         )
     } else {
         unimplemented!()
     };
 
+    let bound = if !input.attrs.is_empty() && input.attrs.len() == 1 {
+        match &input.attrs[0].meta {
+            Meta::List(ls) => {
+                let mut map = parse_annotations(&ls.tokens)?;
 
-    let generics = &input.generics;
+                let bound = map.remove("bound");
 
-    let where_block = if !bound_generics.is_empty() {
-        let bound_params: TokenStream = bound_generics.into_iter().map(|param| {
-            quote!{
-                #param: ::std::fmt::Debug,
-            }
-        }).collect();
+                if let Some((_, (span, _))) = map.into_iter().next() {
+                    return Err(Error::new(span, "Invalid attribute! expected `bound = \"<bound>\""));
+                }
+                bound
+
+            },
+            Meta::Path(_)
+            | Meta::NameValue(_) => return Err(Error::new_spanned(input.attrs[0].meta.clone(), "Only accepts list! #[debug(bound = \"...\")]")),
+        }
+    } else if !input.attrs.is_empty() {
+        return Err(Error::new_spanned(input.attrs[1].clone(), "Can only support 1 or 0 attributes!"));
+    } else {
+        None
+    };
+    
+
+    let where_block = if let Some((_, bound)) = bound {
+        let bound: TokenStream = bound.parse()?;
+
         quote! {
             where
-                #bound_params
+                #bound
         }
     } else {
-        TokenStream::new()
+        let bound_generics = get_uses_of_generics(&input.generics, fields)?;
+        if !bound_generics.is_empty() {
+            let bound_params: TokenStream = bound_generics.into_iter().map(|param| {
+                quote!{
+                    #param: ::std::fmt::Debug,
+                }
+            }).collect();
+            quote! {
+                where
+                    #bound_params
+            }
+        } else {
+            TokenStream::new()
+        }
     };
 
+
+
+    let generics = &input.generics;
     let oth_gen = generics.params.clone().into_iter().map(|a| {
         match a {
             syn::GenericParam::Lifetime(li) => quote!(#li,),
@@ -68,6 +101,69 @@ fn derive_impl(input: DeriveInput) -> Result<TokenStream> {
             }
         }
     })
+}
+
+fn parse_annotations(inp: &TokenStream) -> Result<HashMap<String, (Span, LitStr)>> {
+    fn too_few(last: TokenStream) -> Error {
+        Error::new_spanned(last, "Too few arguments, expected: <name> = \"<info>\"")
+    }
+    let mut tokens = inp.clone().into_iter();
+
+    let mut parsed_tokens: HashMap<String, (Span, LitStr)> = HashMap::new();
+
+    loop {
+        let name = match tokens.next() {
+            Some(val) => val,
+            None => break,
+        };
+
+        let equals = tokens.next().ok_or(too_few(name.clone().into_token_stream()))?;
+
+        let info = tokens.next().ok_or(too_few(TokenStream::from_iter(vec![name.clone(),equals.clone()])))?;
+
+
+        let name_ident = match name {
+            TokenTree::Ident(ident) => ident,
+            TokenTree::Group(_)
+            | TokenTree::Punct(_)
+            | TokenTree::Literal(_) => return Err(Error::new_spanned(name.clone(), "Expected literal!")),
+        };
+
+        match equals {
+            TokenTree::Punct(punc) => if punc.as_char() != '=' {
+                return Err(Error::new_spanned(punc.clone(), "Expected `=` !"));
+            },
+            TokenTree::Ident(_)
+            | TokenTree::Literal(_)
+            | TokenTree::Group(_) => return Err(Error::new_spanned(equals.clone(), "Expected `=` !")),
+
+        }
+
+        let info_str = match info {
+            TokenTree::Group(_)
+            | TokenTree::Ident(_)
+            | TokenTree::Punct(_) => return Err(Error::new_spanned(info.clone(), "Expected string literal! \"<info>\"")),
+            TokenTree::Literal(lit) => syn::parse2::<LitStr>(lit.into_token_stream())?,
+        };
+
+        //eprintln!("{} = {}", name_ident, info_str.to_token_stream());
+
+        parsed_tokens.insert(name_ident.to_string(), (name_ident.span(), info_str));
+
+
+
+        match tokens.next() {
+            Some(val) => match val {
+                TokenTree::Punct(punc) => if punc.as_char() != ',' {
+                    return Err(Error::new_spanned(punc.clone(), "Expected `,`!"));
+                },
+                _ => return Err(Error::new_spanned(val.clone(), "Expected `,`!"))
+            },
+            None => break,
+        }
+    }
+
+    Ok(parsed_tokens)
 }
 
 fn get_uses_of_generics(generics: &Generics, fields: &Fields) -> Result<Vec<Type>> {
@@ -147,6 +243,7 @@ fn unwrap_generic(gen: &GenericArgument) -> Result<Option<&Type>> {
         _ => Ok(None),
     }
 }
+
 fn derive_struct(name: &str, input: &DataStruct) -> Result<TokenStream> {
     let args: TokenStream = input.fields.iter().map(|field| {
         let field_name = field.ident.clone().unwrap();
--- main.rs
+++ main.rs
@@ -14,21 +14,28 @@ pub trait Trait {
 }
 
 #[derive(CustomDebug)]
-pub struct Field<T: Trait> {
+#[debug(bound = "T::Value: Debug")]
+pub struct Wrapper<T: Trait> {
+    field: Field<T>,
+    #[debug = "0b{:08b}"]
+    bitmask: u8,
+}
+
+#[derive(CustomDebug)]
+struct Field<T: Trait> {
     values: Vec<T::Value>,
 }
 
 fn assert_debug<F: Debug>() {}
 
 fn main() {
-    // Does not implement Debug, but its associated type does.
     struct Id;
 
     impl Trait for Id {
         type Value = u8;
     }
 
-    assert_debug::<Field<Id>>();
+    assert_debug::<Wrapper<Id>>();
 }
 
 
