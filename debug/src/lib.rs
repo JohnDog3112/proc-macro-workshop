d7c0c57a2b986e5add11919e3c0ddd444d6c0aaa
John-laptop <johndog3112@gmail.com>
2024-01-24 21:35:02 -0600

    Debug 03

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,47 +1,80 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Data, DataStruct};
+use syn::{parse_macro_input, DeriveInput, Data, DataStruct, Result, Error, Meta, Expr, spanned::Spanned, Lit};
 
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-
     let input = parse_macro_input!(input as DeriveInput);
 
+
+    derive_impl(input)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
+    
+}
+
+fn derive_impl(input: DeriveInput) -> Result<TokenStream> {
     
     let name = &input.ident;
     let name_str = name.to_string();
 
     let implementation = if let Data::Struct(val) = &input.data {
-        derive_struct(&name_str, val)
+        derive_struct(&name_str, val)?
     } else {
         unimplemented!()
     };
 
 
-    quote!{
+    Ok(quote!{
         impl ::std::fmt::Debug for #name {
             fn fmt(&self, fmt: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
                 #implementation
             }
         }
-    }.into()
+    })
 }
 
 
-fn derive_struct(name: &str, input: &DataStruct) -> TokenStream {
+fn derive_struct(name: &str, input: &DataStruct) -> Result<TokenStream> {
     let args: TokenStream = input.fields.iter().map(|field| {
         let field_name = field.ident.clone().unwrap();
         let field_str = field_name.to_string();
         
-        quote! {
-            .field(#field_str, &self.#field_name)
-        }
-    }).collect();
+        if !field.attrs.is_empty() {
+            //eprintln!("{}", field.attrs[0].meta.clone().into_token_stream());
+            let meta = if let Meta::NameValue(name) = &field.attrs[0].meta {
+                Ok(name)
+            } else {
+                Err(Error::new_spanned(field.attrs[0].meta.clone(), "Expected `debug = \"...\"`"))
+            }?;
 
-    quote!{
+            let literal = if let Expr::Lit(lit) = &meta.value {
+                Ok(lit)
+            } else {
+                Err(Error::new(meta.value.span(), "Expected a String Literal! `debug = \"...\"`"))
+            }?;
+
+            let str_literal = if let Lit::Str(st) = &literal.lit {
+                Ok(st)
+            } else {
+                Err(Error::new(literal.lit.span(), "Expected a String Literal! `debug = \"...\"`"))
+            }?;
+
+
+            Ok(quote!{
+                .field(#field_str, &format_args!(#str_literal, &self.#field_name))
+            })
+        } else {
+            Ok(quote! {
+                .field(#field_str, &self.#field_name)
+            })
+        }
+    }).collect::<std::result::Result<TokenStream, Error>>()?;
+
+    Ok(quote!{
         fmt.debug_struct(#name)
             #args
             .finish()
-    }
+    })
 }
--- main.rs
+++ main.rs
@@ -8,9 +8,11 @@
 
 use derive_debug::CustomDebug;
 
+
 #[derive(CustomDebug)]
 pub struct Field {
     name: &'static str,
+    #[debug = "0b{:08b}"]
     bitmask: u8,
 }
 
@@ -21,8 +23,9 @@ fn main() {
     };
 
     let debug = format!("{:?}", f);
+    let expected = r#"Field { name: "F", bitmask: 0b00011100 }"#;
 
-    assert!(debug.starts_with(r#"Field { name: "F","#));
+    assert_eq!(debug, expected);
 }
 
 
