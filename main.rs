42c473b784da867e1ccd614dc9c11a556be4f1b0
John-laptop <johndog3112@gmail.com>
2024-01-26 15:50:02 -0600

    seq 09 (just worked?)

--- main.rs
+++ main.rs
@@ -8,8 +8,37 @@
 
 use seq::seq;
 
-seq!(N in 0..1 {
-    fn main() {
-        let _ = Missing~N;
+
+macro_rules! pass_nproc {
+    ($mac:ident) => {
+        $mac! { 256 }
+    };
+}
+
+macro_rules! literal_identity_macro {
+    ($nproc:literal) => {
+        $nproc
+    };
+}
+
+// Expands to: `const NPROC: usize = 256;`
+const NPROC: usize = pass_nproc!(literal_identity_macro);
+
+struct Proc;
+
+impl Proc {
+    const fn new() -> Self {
+        Proc
     }
-});
+}
+
+macro_rules! make_procs_array {
+    ($nproc:literal) => {
+        seq!(N in 0..$nproc { [#(Proc::new(),)*] })
+    }
+}
+
+// Expands to: `static PROCS: [Proc; NPROC] = [Proc::new(), ..., Proc::new()];`
+static PROCS: [Proc; NPROC] = pass_nproc!(make_procs_array);
+
+fn main() {}
