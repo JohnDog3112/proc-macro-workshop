a3bbb1ae26da23b7198d6d2ee58bd05f959892b3
John-laptop <johndog3112@gmail.com>
2024-01-27 11:21:51 -0600

    sorted 01

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,24 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+use syn::{Result, Error, parse_macro_input};
+
+type ProcStream = proc_macro::TokenStream;
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn sorted(args: ProcStream, input: ProcStream) -> ProcStream {
     let _ = args;
-    let _ = input;
+    let inp = input;
 
-    unimplemented!()
+    let item = parse_macro_input!(inp as syn::Item);
+
+    sorted_impl(args, item)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
+}
+
+
+fn sorted_impl(args: ProcStream, input: syn::Item) -> Result<TokenStream> {
+    eprintln!("{:?}", input);
+
+    Ok(input.into_token_stream())
 }
