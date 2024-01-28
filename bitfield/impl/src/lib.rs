12feae9f82329d23769468b4110cb29c7cd57fbe
John-laptop <johndog3112@gmail.com>
2024-01-27 18:19:24 -0600

    bitfield 03

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -1,4 +1,4 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{TokenStream, Ident};
 use quote::quote;
 use syn::{parse_macro_input, Item, Result, Error, ItemStruct};
 
@@ -34,18 +34,126 @@ fn bitfield_struct(_args: ProcStream, struc: ItemStruct) -> Result<TokenStream> 
     let vis = struc.vis;
     let ident = struc.ident;
 
-    let size: TokenStream = struc.fields.into_iter().map(|field| {
-        let ty = field.ty;
+    let size: TokenStream = struc.fields.iter().map(|field| {
+        let ty = &field.ty;
         quote!(
             + #ty::BITS
         )
     }).collect();
 
+    let mut prev: TokenStream = quote!(0);
 
+    let getters_setters: TokenStream = struc.fields.iter().map(|field| {
+        let field_ident = field.ident.clone().unwrap();
+        let ty = &field.ty;
+        let setup = quote!(
+            const prev_bits: usize = #prev;
+
+            const start_bytes: usize = prev_bits/8;
+            const start_bits: usize = prev_bits%8;
+
+            const last_bits: usize = (prev_bits + #ty::BITS);
+            
+            const end_bytes: usize = last_bits/8;
+            const end_bits: usize = last_bits%8;
+        );
+
+        prev.extend(quote!(+ #ty::BITS));
+
+        let getter = Ident::new(
+            &format!("get_{field_ident}"), field_ident.span()
+        );
+
+        let setter = Ident::new(
+            &format!("set_{field_ident}"), field_ident.span()
+        );
+
+        quote!(
+            #vis fn #getter(&self) -> u64 {
+                #setup
+
+                if start_bytes == end_bytes {
+                    const start_mask: u8 = (0b1111_1111u8.overflowing_shr(start_bits as u32)).0;
+                    const end_mask: u8 = (0b1111_1111u8.overflowing_shl((8-end_bits) as u32)).0;
+
+                    return ((self.data[start_bytes] & start_mask & end_mask) >> (8 - end_bits)) as u64;
+                }
+
+                const mask: u8 = (0b1111_1111 >> start_bits);
+                let mut total: u64 = (self.data[start_bytes] & mask) as u64;
+
+
+                #[allow(clippy::reversed_empty_ranges)]
+                for byte in start_bytes+1..end_bytes {
+                    total = total.overflowing_shl(8 as u32).0 + self.data[byte] as u64;
+                }
+
+                if start_bytes != end_bytes && end_bits != 0{
+                    (total << end_bits) + self.data[end_bytes].overflowing_shr((8 - end_bits) as u32).0 as u64
+                    
+                } else {
+                    total
+                }
+            }
+
+            #vis fn #setter(&mut self, mut val: u64) {
+                #setup
+
+
+                if start_bytes == end_bytes || (start_bytes+1 == end_bytes && end_bits == 0) {
+                    const mask: u8 = 0b1111_1111u8.overflowing_shr(start_bits as u32).0
+                        & 0b1111_1111u8.overflowing_shl((8-end_bits) as u32).0;
+
+                    const rev_mask: u8 = mask ^ 0b1111_1111;
+
+                    const val_mask: u8 = 0b1111_1111u8.overflowing_shr((8 - end_bits+start_bits) as u32).0;
+
+                    self.data[start_bytes] = (self.data[start_bytes] & rev_mask)
+                        | ((val as u8) & val_mask).overflowing_shl((8-end_bits) as u32).0;
+                    
+                    return;
+                }
+
+
+                if end_bits != 0 {
+                    const end_mask_val: u8 = 0b1111_1111u8.overflowing_shr((8-end_bits) as u32).0;
+                    const end_mask_data: u8 = 0b1111_1111u8.overflowing_shr(end_bits as u32).0;
+
+
+                    self.data[end_bytes] = (((val as u8) & end_mask_val) << (8-end_bits))
+                        | (self.data[end_bytes] & end_mask_data);
+                    
+                    val >>= end_bits;
+                }
+
+                #[allow(clippy::reversed_empty_ranges)]
+                for byte in (start_bytes+1..end_bytes).rev() {
+                    self.data[byte] = (val & 0b1111_1111) as u8;
+                    val >>= 8;
+                }
+
+                const start_mask_val: u8 = 0b1111_1111u8.overflowing_shr(start_bits as u32).0;
+                const start_mask_data: u8 = start_mask_val ^ 0b1111_1111;
+
+                self.data[start_bytes] = ((val as u8) & start_mask_val) | (self.data[start_bytes] & start_mask_data);
+                
+            }
+        )
+
+    }).collect();
     Ok(
         quote!(
             #vis struct #ident {
                 data: [u8; (0 #size)/8]
+            }
+
+            impl #ident {
+                fn new() -> Self {
+                    Self {
+                        data: [0; (0 #size)/8]
+                    }
+                }
+                #getters_setters
             }
         )
     )
--- main.rs
+++ main.rs
@@ -13,9 +13,37 @@ pub struct MyFourBytes {
     a: B1,
     b: B3,
     c: B4,
+    p1: B3,
     d: B24,
+    p2: B5,
 }
 
+/*pub struct MyFourBytes {
+    data: [u8; 4]
+}*/
+
+
+
 fn main() {
-    assert_eq!(std::mem::size_of::<MyFourBytes>(), 4);
+    let mut bitfield = MyFourBytes::new();
+    assert_eq!(0, bitfield.get_a());
+    assert_eq!(0, bitfield.get_b());
+    assert_eq!(0, bitfield.get_c());
+    assert_eq!(0, bitfield.get_d());
+
+    bitfield.set_d(0b1001_1011__1101_0111__1110_1101);
+
+    println!("[{}]", bitfield.data.iter().map(|a| format!("0b{:08b}", a)).collect::<Vec<String>>().join(", "));
+
+    bitfield.data[0] = 0b1111_0100;
+
+    bitfield.data[1] = 0b1111_1001;
+    bitfield.data[2] = 0b1010_1111;
+    bitfield.data[3] = 0b1111_1001;
+
+    println!("0b{:01b}", bitfield.get_a());
+    println!("0b{:03b}", bitfield.get_b());
+    println!("0b{:04b}", bitfield.get_c());
+
+    println!("0b{:024b}", bitfield.get_d());
 }
